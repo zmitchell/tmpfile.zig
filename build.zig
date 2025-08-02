3ebbee73ba8adfa81bec1f88b79abe28cbad27ca
Zach Mitchell <zmitchell@fastmail.com>
2025-08-01 20:22:49 -0600

    refactor: module filename

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
         // only contains e.g. external object files, you can make this `null`.
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/tmpfile.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
