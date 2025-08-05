5e73de9ed295056b38af948b66da850d107feea4
Zach Mitchell <zmitchell@fastmail.com>
2025-08-04 21:53:05 -0600

    fix: expose tmpfile module

--- build.zig
+++ build.zig
@@ -18,14 +18,10 @@ pub fn build(b: *std.Build) void {
     // This creates a "module", which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    const lib_mod = b.addModule("tmpfile", .{
         .root_source_file = b.path("src/tmpfile.zig"),
         .target = target,
-        .optimize = optimize,
+        .optimize = optimize
     });
 
     // Now, we will create a static library based on the module we created above.
